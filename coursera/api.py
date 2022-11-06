d67b9d86de9e028549809425323e32dca63073b3
Raffaele Mancuso <54762742+raffaem@users.noreply.github.com>
2022-11-06 16:04:22 +0100

    Switch to V2 of coursera API

--- coursera/api.py
+++ coursera/api.py
@@ -315,11 +315,11 @@ class OnDemandCourseMaterialItemsV1(object):
         @rtype: OnDemandCourseMaterialItems
         """
 
-        dom = get_page(session, OPENCOURSE_ONDEMAND_COURSE_MATERIALS,
+        dom = get_page(session, OPENCOURSE_ONDEMAND_COURSE_MATERIALS_V2,
                        json=True,
                        class_name=course_name)
         return OnDemandCourseMaterialItemsV1(
-            dom['linked']['onDemandCourseMaterialItems.v1'])
+            dom['linked']['onDemandCourseMaterialItems.v2'])
 
     def get(self, lesson_id):
         """
