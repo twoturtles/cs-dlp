11bda6da894a1afac5334f3011c01d01ec497cfe
Raffaele Mancuso <54762742+raffaem@users.noreply.github.com>
2023-06-15 13:03:19 +0200

    Reorder files

--- cs-dlp/cs-dlp.bat
+++ cs-dlp/cs-dlp.bat
@@ -9,4 +9,4 @@ rem location of this .bat file) via Python, passing all user-specified arguments
 
 rem Fixed the issue of spaces in the PATH name adding quotes before the batch Windows commands for drive and path. It should work fine now.
 rem This issue was tested with pretty nasty path names (i.e. "t h i s i s t e r r i b l e") and it worked.
-python "%~dp0\coursera-dl" %*
+python "%~dp0\cs-dlp" %*
